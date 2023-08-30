9e21bee00916845248c14477113977893bb1befd
gilliankb <gillian.keusch@student-cs.fr>
2023-08-30 16:22:01 +0200

    0.16

--- sleepQuiz.js
+++ sleepQuiz.js
@@ -1,13 +1,3 @@
-
-// FORM DIVS BUILD
-
-const lottieSrc = "https://lottie.host/186d5b64-22b9-4c26-9068-8d0b40cbef57/UC2tpZmJA6.json"
-const slackUrl = "https://europe-west1-test-firebase-1240d.cloudfunctions.net/postSlackMessage";
-const formPushUrl = "https://europe-west1-test-firebase-1240d.cloudfunctions.net/sleepQuiz";
-
-const questions = document.getElementsByClassName("form-question");
-const sliderContainer = document.getElementById("slider");
-
 // Remove submit with enter
 $(document).keypress(
     function(event){
@@ -15,6 +5,21 @@ $(document).keypress(
         event.preventDefault();
     }
 });
+
+
+const lottieSrc = "https://lottie.host/186d5b64-22b9-4c26-9068-8d0b40cbef57/UC2tpZmJA6.json"
+const mappingLifeMoment = {
+    "Menstruation": 5,
+    "Trying to conceive": 6,
+    "Pregnancy": 7,
+    "Post Partum": 8,
+    "Menopause": 9,
+    "Peri Menopause": 9,
+    "Other": 10
+}
+
+const questions = document.getElementsByClassName("form-question");
+const sliderContainer = document.getElementById("slider");
 
 // Add necessary slides
 const hiddenSlide = document.getElementsByClassName("w-slide")[1];
@@ -71,108 +76,101 @@ for (let i = 0; i < slides.length; i++) {
     input.setAttribute("value", value);
     });
 
-    slide.querySelector("#next-button")?.setAttribute("name", `${i}`);
-    slide.querySelector("#previous-button")?.setAttribute("slideIndex", `${i}`);
-
     // NUMBER INPUTS
     slide.querySelector("input[type='number']")?.setAttribute("data-name", `${i}`);
     slide.querySelector("input[type='number']")?.setAttribute("name", `${i}`);
 }
 
+function getCurrentSlideIndex() {
+    const currentSlide = document.querySelector(".w-slide:not([aria-hidden])");
+    return parseInt(currentSlide.getAttribute("aria-label").split(" ")[0]);
+}
 
-function changeHeader(mode) {
-    if (mode == "logo") {
-    logo.style.display = 'block';
-    title.style.display = 'none';
-    progressBar.style.display = 'none';
+
+// Show logo if feedback slide
+const feedbackSlides = [5, 6, 7, 8, 9, 10, 16, 20, 22, 26, 27];
+
+function changeHeader() {
+    const currentSlideIndex = getCurrentSlideIndex();
+    if (feedbackSlides.includes(currentSlideIndex)) {
+        
+        // display logo
+        logo.style.display = 'block';
+        title.style.display = 'none';
+        progressBar.style.display = 'none';
+
+        // change header color
+        document.body.style.backgroundColor = "#f6f4ef";
+        document.querySelector('#form-header').style.borderColor = "#D4D4D2";
+        document.querySelector('#form-header').style.borderWidth = "0px 0px 1px 0px"
     } else {
-    logo.style.display = 'none';
-    title.style.display = 'block';
-    progressBar.style.display = 'block';
+        logo.style.display = 'none';
+        title.style.display = 'block';
+        progressBar.style.display = 'block';
+
+        document.body.style.backgroundColor = "#ffffff";
+        document.querySelector('#form-header').style.borderWidth = "0px 0px 0px 0px"
     }
 }
-// Initially put the progress bar
-changeHeader("bar");
+// Initially put the map
+changeHeader();
 
 function changeTitle(title) {
     document.getElementById("title").innerHTML = title;
 }
 
-const mappingLifeMoment = {
-    "Trying to conceive": 6,
-    "Pregnancy": 7,
-    "Post Partum": 8,
-    "Menopause": 9,
-    "Menstruation": 5,
-    "Peri Menopause": 9,
-    "Other": 10
+function showSlide(index) {
+    $(`#flowbaseSlider div:nth-child(${index})`).trigger('tap');
 }
-const feedbackSlides = ["4", "5", "6", "7", "8", "15", "19", "21", "25", "26"];
-const lifemomentSlides = ["4", "5", "6", "7", "8", "9", "10"];
 
 function updateLoadingBar() {
-    const currentSlide = document.querySelector(".w-slide:not([aria-hidden])");
-    const currentSlideIndex = parseInt(currentSlide.getAttribute("aria-label").split(" ")[0]);
-    // Between 1-10 : 0 - 25% -> 10 ELEMENTS
-    // Between 11-27 : 25 - 50% -> 16 ELEMENTS
+    const currentSlideIndex = getCurrentSlideIndex();
     const percentage = Math.round(
         currentSlideIndex <= 9 ? currentSlideIndex * 25 / 8 : 25 + (currentSlideIndex - 10) * 25 / 16
     )
     document.getElementById('loading-bar').setAttribute("style",`display:flex;width:${percentage}%`);
     document.getElementById('loading-bar').style.width=`${percentage}%`;
 }
+
+// Initialize loading bar
 updateLoadingBar();
 
 // Automate next slide
 const goToNextSlide = function(input) {
-    updateLoadingBar();
-    if (input.name == slides.length) {
-    return ;
+    let currentSlideIndex = getCurrentSlideIndex();
 
-    // Go to next page
-    } else if (input.name == "3") {
-    showSlide(mappingLifeMoment[input.value]);
-    } else if (lifemomentSlides.includes(input.name)) {
-    showSlide(11);
-    changeTitle("SLEEP PROFILE");
+    console.log("current slide Index :", currentSlideIndex);
+
+    if (currentSlideIndex == slides.length) {
+        return;
+    }else if (currentSlideIndex == 4) {
+        showSlide(mappingLifeMoment[input.value]);
+    } else if ((4 <= currentSlideIndex) && (currentSlideIndex <= 10)) {
+        showSlide(11);
+        changeTitle("SLEEP PROFILE");
     } else {
-    rightArrow.click();
+        showSlide(currentSlideIndex + 1)
     };
+    updateLoadingBar();
+    changeHeader();
 
-    // Update header
-    if (feedbackSlides.includes(input.name)) {
-    changeHeader("bar");
-    } else if (feedbackSlides.includes(String(parseInt(input.name) + 1))) {
-    changeHeader("logo");
-    };
 };
 
     // Automate next slide
 const goToPreviousSlide = function() {
-    const currentSlide = document.querySelector(".w-slide:not([aria-hidden])");
-    const currentSlideIndex = parseInt(currentSlide.getAttribute("aria-label").split(" ")[0]) - 1;
+    const currentSlideIndex = getCurrentSlideIndex();
     if (currentSlideIndex == 0) {
     return ;
-    }
-    if (lifemomentSlides.includes(String(currentSlideIndex))) {
+    } else if ((5 <= currentSlideIndex) && (currentSlideIndex <= 11)) {
         showSlide(4);
+        changeTitle("DEMOGRAPHIC PROFILE");
     } else {
-        leftArrow.click();
-    };
-    if (feedbackSlides.includes(String(currentSlideIndex))) {
-    changeHeader("bar");
-    } else if (feedbackSlides.includes(String(currentSlideIndex - 1))) {
-    changeHeader("logo");
-    }
-    if (currentSlideIndex == 9) {
-    changeTitle("DEMOGRAPHIC PROFILE");
+        showSlide(currentSlideIndex - 1)
     }
     updateLoadingBar();
+    changeHeader();
 };
 
-function showSlide(index) {
-    $(`#flowbaseSlider div:nth-child(${index})`).trigger('tap');
-}
 
 // Add event listeners to buttons
 $('.next-button, .final-button').on('click', function(){
@@ -216,16 +214,7 @@ $('input[type="radio"]').on('tap', function(){
 $('input[type="number"]').attr("min", "0");
 
 
-// Focus on next if enter is clicked
-const numberInputs = document.querySelectorAll('input[type="number"]');
-numberInputs.forEach(input => {
-    input.addEventListener("keypress", (e) => checkKey(e, input));
-    input.addEventListener("input", (e) => checkValue(e, input));
-    let nextButton = input.parentNode.parentNode.getElementsByTagName("a")[0];
-    nextButton.style.backgroundColor = "#dfdfdf";
-    nextButton.setAttribute("disabled", "disabled");
-});
-
+// Add listeners to numerical inputs
 function checkValue(e, input) {
     let nextButton = input.parentNode.parentNode.getElementsByTagName("a")[0];
     if (input.value === '') {
@@ -242,13 +231,22 @@ function checkKey(e, input) {
     }
 };
 
-// Post function
+const numberInputs = document.querySelectorAll('input[type="number"]');
+numberInputs.forEach(input => {
+    input.addEventListener("keypress", (e) => checkKey(e, input));
+    input.addEventListener("input", (e) => checkValue(e, input));
+    let nextButton = input.parentNode.parentNode.getElementsByTagName("a")[0];
+    nextButton.style.backgroundColor = "#dfdfdf";
+    nextButton.setAttribute("disabled", "disabled");
+});
 
+// Post function
 async function postRequest(url, data) {
     const response = await fetch(url, {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)});
 }
 
 function sendSlack(submitted, email) {
+    const slackUrl = "https://europe-west1-test-firebase-1240d.cloudfunctions.net/postSlackMessage";
     let data = {
         userName: email,
         warningType: 'QUIZ',
@@ -259,12 +257,8 @@ function sendSlack(submitted, email) {
     postRequest(slackUrl, data);
 }
 
-// Progress bar animation
+// Just before last view : progress bar animation
 $('#lottie-container').html(`<lottie-player autoplay mode="normal" speed=1 style="width: 400px"></lottie-player>`);
-
-
-
-// Last button before email
 $('#final-button').on('click', function() {
     // Display animation container
     let loaderContainer = document.getElementById("final-progress");
@@ -281,17 +275,20 @@ $('#final-button').on('click', function() {
     sendSlack(false, "Unknown User");
 })
 
+
 // After email input
-$('.email-next-button').on('click', function(){
+$('.email-next-button').on('click', validateEmailForm);
+
+function validateEmailForm() {
+    const formPushUrl = "https://europe-west1-test-firebase-1240d.cloudfunctions.net/postForm";
 
     // Check for errors
     let error = "";
     const inputs = $('input:not([aria-hidden])');
     const emailInputs = $(inputs).filter('[type="email"]').toArray();
     if (emailInputs.length > 0 && !emailInputs.some((el) => $(el).val().indexOf('@') !== -1 && $(el).val().indexOf('.') !== -1)) {
-          error = "Please enter a valid email";
+        error = "Please enter a valid email";
     }
-
     // If not error -> submit form
     if (!error) {
         sendSlack(true, emailInputs[0].value);
@@ -299,8 +296,8 @@ $('.email-next-button').on('click', function(){
         document.forms[0].submit();
         error = "";
     }
-    
-    else {
-    alert(error);
-    }
-});
+     
+     else {
+     alert(error);
+     }
+}
