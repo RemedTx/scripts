3043f02a36f2c1ca45bf79aa54d3307dc5db6e48
gilliankb <gillian.keusch@student-cs.fr>
2023-09-01 17:25:11 +0200

    0.22

--- sleepQuiz.js
+++ sleepQuiz.js
@@ -301,71 +301,43 @@ function validateEmailForm() {
      }
 }
 
+postFormUrl = "https://europe-west1-test-firebase-1240d.cloudfunctions.net/testFormSubmit";
 
-const FormAbandonmentTracker = {
-    init: function(form_id, event_category, event_action = 'FormAbandonment') {
-      this.$eventCategory = event_category || form_id;
-      this.$eventAction = event_action;
-  
-      this.$formHistory = [];
-      this.$formIsSubmitted = false;
-      this.$formId = form_id;
-      this.$form = document.getElementById(form_id);
-  
-      if(!this.$form) {
-        console.error("FormTracker Error: Please specify the correct form id when initializing FormTracker.");
-        return;
-      }
-      this.attachEvents();
-    },
-    attachEvents: function() {
-      let that = this;
-      this.$form.querySelectorAll('select').forEach(function(el) {
-        el.addEventListener('change', function(e) { return that.onFieldChange(e); });
-      });
-      this.$form.querySelectorAll('input, textarea').forEach(function(el) {
-        el.addEventListener('input', function(e) { return that.onFieldInput(e); });
-      });
-      this.$form.addEventListener('submit', function(e) { return that.onFormSubmit() });
-      window.addEventListener('beforeunload', function(e) { return that.onFormAbandonment() } );
-    },
-    onFieldChange: function(event) {
-      let field_id = event.target.id;
-      this.addFieldToHistory(field_id);
-    },
-    onFieldInput: function(event) {
-      let field_id = event.target.id;
-      this.addFieldToHistory(field_id);
-    },
-    addFieldToHistory: function(field_id) {
-      this.$formHistory.push(field_id);
-      this.$formHistory = this.$formHistory.filter((v, i, a) => a.indexOf(v) === i).sort();
-    },
-    onFormSubmit: function() {
-      this.$formIsSubmitted = true;
-      this.clearFormHistory();
-    },
-    onFormAbandonment: function() {
-      if(!this.$formIsSubmitted) {
-        this.sendEvents();
-      }
-    },
-    sendEvents: function() {
-      let joined_history = this.$formHistory.join(', ');
-      // Send the data off to Google
-    //   this.$gtag('event', this.$eventAction, {
-    //     'event_category': this.$eventCategory,
-    //     'event_label': 'Fields with input: ' + joined_history,
-    //     'event_callback': function() {
-    //     }
-    //   });
-      postRequest("https://europe-west1-test-firebase-1240d.cloudfunctions.net/postForm", {sheet: 0, data: joined_history});
-    },
-    clearFormHistory: function() {
-      this.$formHistory = [];
-    }
-  };
+  const FormAbandonmentTracker2 = {
+      init: function(form_id) {
+      	this.$date = new Date().toLocaleString();
+        this.$formHistory = [];
+        this.$form = document.getElementById(form_id);
+        this.attachEvents();
+      },
+      attachEvents: function() {
+        let that = this;
+        this.$form.querySelectorAll('select').forEach(function(el) {
+          el.addEventListener('change', function(e) { return that.onFieldChange(e); });
+        });
+        this.$form.querySelectorAll('input, textarea').forEach(function(el) {
+          el.addEventListener('input', function(e) { return that.onFieldChange(e); });
+        });
+        window.addEventListener('visibilitychange', function(e) { return that.onFormAbandonment() } );
+      },
+      onFieldChange: function(event) {
+      	// Gets the index of the question slide
+        let name = event.target.parentNode.parentNode.parentNode.parentNode.parentNode;
+        let index = name.getAttribute("aria-label").split(" ")[0];
+        this.$formHistory.push(parseInt(index));
+      },
+      onFormAbandonment: function() {
+        if(!this.$formIsSubmitted  && document.visibilityState == "hidden") {
+          this.sendEvents();
+        }
+      },
+      sendEvents: function() {
+        let lastQuestion = Math.max(...this.$formHistory);
+        console.log("Last Question : ", lastQuestion);
+        navigator.sendBeacon(postFormUrl, JSON.stringify({sheet: 1, data: [this.$date, lastQuestion]}));
+      },
+    };
 
-(function(){
-FormAbandonmentTracker.init('onboarding-form', 'Onboarding Form', 'FormAbandonment');
-})();
+  (function(){
+    FormAbandonmentTracker2.init('onboarding-form');
+  })();
